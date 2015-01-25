9b5d6c850bac8ca03875a0d988f9717457bde8ec
VIP <v.i.p98@indamail.hu>
2015-01-25 13:49:58 +0100

    ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##So this is my function. Honestly I don't really get the point of
+## that task. I just wrote a function like that we can saw in the 
+##example. It works same... So i hope that is good. :D
+## (I just changed some words in the example.)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL       ## from the example, inv = m
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(i) inv <<- i  ## from the example i=mean
+    getinv <- function() inv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)    
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()  ## from the example too: "m <- x$getmean()"
+    if(!is.null(inv)) {
+        message("getting cached inverse")
+        return(inv)
+    }
+    matrix <- x$get() ## that is too, matrix = data
+    inv <- solve(matrix, ...)
+    x$setinv(inv)  ## from the example...
+    inv
 }
